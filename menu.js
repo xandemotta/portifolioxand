4b8dbd8c8cdd9a007fe729e61bcf82bcf5720f1a
xandemotta <85975693+xandemotta@users.noreply.github.com>
2024-05-14 03:26:01 -0300

    mudanças no JS e no php

--- menu.js
+++ menu.js
@@ -12,3 +12,29 @@ menu.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
+
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que o formulário seja enviado normalmente
+
+    // Obtém os dados do formulário
+    const formData = new FormData(this);
+
+    // Envia os dados para o arquivo PHP usando AJAX
+    fetch('enviar_email.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.text(); // Retorna o texto da resposta
+        }
+        throw new Error('Erro ao enviar o email.');
+    })
+    .then(data => {
+        alert(data); // Exibe a mensagem de sucesso ou erro
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        alert('Erro ao enviar o email.');
+    });
+});
